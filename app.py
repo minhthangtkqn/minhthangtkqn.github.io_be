acaa0e2aaefda2e18da4a9bd278f55b67e1f5edc
Hoàng Minh Thắng <hoangminhthang21051995@gmail.com>
2022-01-17 00:21:56 +0700

    Update: CRUD element on db

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from crypt import methods
+from urllib import response
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -74,12 +75,41 @@ def handle_cars():
     elif request.method == 'GET':
         cars = CarsModel.query.all()
         result = [{
+            "id": car.id,
             "name": car.name,
             "model": car.model,
             "doors": car.doors
         } for car in cars]
 
         return {'count': len(result), 'cars': result}
+
+
+@app.route('/cars/<car_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_car(car_id):
+    car = CarsModel.query.get_or_404(car_id)
+
+    if request.method == 'GET':
+        response = {
+            "id": car.id,
+            "name": car.name,
+            "model": car.model,
+            "doors": car.doors
+        }
+        return {"message": "success", "car": response}
+
+    elif request.method == 'PUT':
+        data = request.get_json()
+        car.name = data['name']
+        car.model = data['model']
+        car.doors = data['doors']
+        db.session.add(car)
+        db.session.commit()
+        return {"message": f"car {car.name} successfully updated."}
+
+    elif request.method == 'DELETE':
+        db.session.delete(car)
+        db.session.commit()
+        return {"message": f"car {car.name} successfully deleted."}
 
 if __name__ == '__main__':
     app.run()
